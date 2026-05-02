c7f814af3bc5871327709ca9dd09166137fe5d16
Alex <muitaalexander19@gmail.com>
2026-05-02 12:29:39 +0300

    added a random 3 number generation section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+  //counting by increasing and decreasing.
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
@@ -16,4 +18,27 @@ increaseBtn.onclick = function(){
 resetBtn.onclick = function(){
        count = 0;
        countLabel.textContent = count;
+}
+
+
+//random number generation
+const randomBtn = document.getElementById("randomBtn");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+let max = 100;
+let min = 1;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+
+randomBtn.onclick = function(){
+  randomNum1 = Math.floor(Math.random() * max)+ min;
+  randomNum2 = Math.floor(Math.random() * max)+ min;
+  randomNum3 = Math.floor(Math.random() * max)+ min;
+
+  label1.textContent = `random number 1: ${randomNum1}`;
+  label2.textContent = `random number 2: ${randomNum2}`;
+  label3.textContent = `random number 3: ${randomNum3}`;
 }
